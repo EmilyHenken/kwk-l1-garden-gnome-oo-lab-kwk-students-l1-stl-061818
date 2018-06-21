dd04232337ba561979237b5d2a584ed0ede22f23
EmilyHenken <20henkee@corjesu.org>
2018-06-21 20:35:37 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,7 +1,14 @@
 class GardenGnome
 
-  def initialize(personality, )
-    @personality = evil
-    @
+  class Book
+  attr_accessor :author, :page_count, :genre 
+  attr_reader :title 
+
+  def initialize(name, age, gluten_allergy)
+    @name = name
+    @age = age
+    @gluten_allergy = gluten_allergy
   end
+  def initialize
+  
 end
